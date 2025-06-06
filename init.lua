a19bdadfd629e1118c9326ebdedb10673a64f66f
Vsevolod Girenko <vs.girenko@mail.ru>
2025-06-06 11:41:04 +0300

    turn on relativenumber

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 -- Enable mouse mode, can be useful for resizing splits for example!
