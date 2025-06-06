f1cac147a3374c0564d5cbbeeac4b3ddc4dcc9cf
Vsevolod Girenko <vs.girenko@mail.ru>
2025-06-06 11:50:13 +0300

    add lang servers for rust and go

--- init.lua
+++ init.lua
@@ -681,10 +681,8 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        gopls = {},
+        rust_analyzer = {},
         phpactor = {
           opts = {
             init_options = {
