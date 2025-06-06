c8abe7bbdb1e946e6e9a784635f0f6cd6ef99f4d
Vsevolod Girenko <vs.girenko@mail.ru>
2025-06-06 11:51:24 +0300

    use goimports as default go formatter
    supports auto importing packages

--- init.lua
+++ init.lua
@@ -784,12 +784,14 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        go = { 'goimports' },
+        -- php = { 'php_cs_fixer' },
       },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
+      --
+      -- You can use 'stop_after_first' to run the first available formatter from the list
+      -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
   },
 
