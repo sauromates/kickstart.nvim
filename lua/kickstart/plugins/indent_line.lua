c36814a5befb65f27b12371c64c166f065c35c02
Vsevolod Girenko <vs.girenko@mail.ru>
2025-06-06 11:58:43 +0300

    thin indent line, no scope underline

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,15 @@
 return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      scope = {
+        enabled = false,
+      },
+      indent = {
+        char = '│',
+      },
+    },
   },
 }
