8f3a37d7bf51a64004816c3971905ffd836beaa2
Vsevolod Girenko <vs.girenko@mail.ru>
2025-11-24 19:23:57 +0300

    remove custom php config

--- init.lua
+++ init.lua
@@ -215,7 +215,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-require 'php'
 require 'terminal'
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
